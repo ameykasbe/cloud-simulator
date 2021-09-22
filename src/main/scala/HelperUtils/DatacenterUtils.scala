d78623af00ac43d5749170a9971b4db90d146568
Kasbe <akasbe2@uic.edu>
2021-09-22 15:38:31 -0500

    Configuration management - Datacenter, host, vm, cloudlet

--- src/main/scala/HelperUtils/DatacenterUtils.scala
+++ src/main/scala/HelperUtils/DatacenterUtils.scala
@@ -21,47 +21,39 @@ import org.cloudbus.cloudsim.provisioners.ResourceProvisionerSimple
 import scala.collection.JavaConverters.*
 
 class DatacenterUtils (schedulerModel: String, vmScheduler: VmScheduler, cloudletScheduler: CloudletScheduler) {
-  // Create a cloudsim object for simulation. Also creates the Cloud Information Service (CIS) entity.
+
   val config = ConfigFactory.load(schedulerModel)
+  val datacenterConfig = new GetDatacenterConfig(schedulerModel)
+  val hostConfig = new GetHostConfig(schedulerModel)
+  val peConfig = new GetPeConfig(schedulerModel)
+  val vmConfig = new GetVmConfig(schedulerModel)
+  val cloudletConfig = new GetCloudletConfig(schedulerModel)
 
   def createDatacenter(cloudsim: CloudSim) = {
-    val numberOfHosts = config.getInt("datacenter.numOfHosts")
-    val hostList = createHost(numberOfHosts)
+    val hostList = createHost(datacenterConfig.numberOfHosts)
     new DatacenterSimple(cloudsim, hostList.asJava, new VmAllocationPolicySimple())
   }
 
   def createHost(numberOfHosts: Int) = {
-    val peList : List[Pe] = createPe()
-    val ram = config.getInt("host.ram")
-    val bw = config.getInt("host.bw")
-    val storage = config.getInt("host.storage")
-    1.to(numberOfHosts).map(x=>new HostSimple(ram, bw, storage, peList.asJava, true).setVmScheduler(vmScheduler)).toList
-
+    val peList : List[Pe] = createPe(hostConfig.numberOfPes)
+    1.to(numberOfHosts).map(x=>new HostSimple(hostConfig.ram, hostConfig.bw, hostConfig.storage, peList.asJava, true).setVmScheduler(vmScheduler)).toList
   }
 
-  def createPe() = {
-    val mipsCapacity = config.getInt("pe.mipsCapacity")
-    val numberOfPes = config.getInt("host.numberOfPe")
-    1.to(numberOfPes).map(x=>new PeSimple(mipsCapacity)).toList
-
+  def createPe(numberOfPes: Int) = {
+    1.to(numberOfPes).map(x=>new PeSimple(peConfig.mipsCapacity)).toList
   }
 
   def createVms() = {
-    val mipsCapacity = config.getInt("vm.mipsCapacity")
-    val ram = config.getInt("vm.ram")
-    val bw = config.getInt("vm.bw")
-    val size = config.getInt("vm.size")
-    val numberOfPes = config.getInt("host.numberOfPe")
-    List(new VmSimple(mipsCapacity, numberOfPes, cloudletScheduler).setRam(ram).setBw(bw).setSize(size))
+    val numberOfPes = hostConfig.numberOfPes
+    val numOfVms = datacenterConfig.numOfVms
+    1.to(numOfVms).map(x => 
+      new VmSimple(vmConfig.mipsCapacity, numberOfPes, cloudletScheduler).setRam(vmConfig.ram).setBw(vmConfig.bw).setSize(vmConfig.size)
+    ).toList
   }
 
   def createCloudlets() = {
-    val numOfCloudlets = config.getInt("datacenter.numofCloudlets")
-    val length = config.getInt("cloudlet.length")
-    val pesNumber = config.getInt("cloudlet.pesNumber")
-    val utilizationModel = new UtilizationModelDynamic(config.getDouble("cloudlet.utilizationModel"))
-    1.to(numOfCloudlets).map(x=>new CloudletSimple(length, pesNumber, utilizationModel)).toList
+    val numOfCloudlets = datacenterConfig.numOfCloudlets
+    1.to(numOfCloudlets).map(x=>new CloudletSimple(cloudletConfig.length, cloudletConfig.pesNumber, cloudletConfig.utilizationModel)).toList
   }
-
 }
 
---
+++ src/main/scala/HelperUtils/GetCloudletConfig.scala
@@ -0,0 +1,30 @@
+package HelperUtils
+
+import Simulations.BasicCloudSimPlusExample.config
+import com.typesafe.config.{Config, ConfigFactory}
+import org.cloudbus.cloudsim.allocationpolicies.VmAllocationPolicySimple
+import org.cloudbus.cloudsim.brokers.DatacenterBrokerSimple
+import org.cloudbus.cloudsim.cloudlets.CloudletSimple
+import org.cloudbus.cloudsim.core.CloudSim
+import org.cloudbus.cloudsim.datacenters.DatacenterSimple
+import org.cloudbus.cloudsim.hosts.HostSimple
+import org.cloudbus.cloudsim.resources.{Pe, PeSimple}
+import org.cloudbus.cloudsim.utilizationmodels.UtilizationModelDynamic
+import org.cloudbus.cloudsim.vms.VmSimple
+import org.cloudsimplus.builders.tables.CloudletsTableBuilder
+import org.cloudbus.cloudsim.schedulers.cloudlet.{CloudletSchedulerAbstract, CloudletSchedulerSpaceShared, CloudletSchedulerTimeShared}
+import org.cloudbus.cloudsim.schedulers.vm
+import org.cloudbus.cloudsim.schedulers.vm.{VmScheduler, VmSchedulerAbstract, VmSchedulerSpaceShared, VmSchedulerTimeShared}
+import org.cloudbus.cloudsim.schedulers.cloudlet.CloudletScheduler
+import org.cloudbus.cloudsim.provisioners.ResourceProvisionerSimple
+
+import scala.collection.JavaConverters.*
+
+class GetCloudletConfig (schedulerModel: String) {
+  // Create a cloudsim object for simulation. Also creates the Cloud Information Service (CIS) entity.
+  val config = ConfigFactory.load(schedulerModel: String)
+  val length = config.getInt("cloudlet.length")
+  val pesNumber = config.getInt("cloudlet.pesNumber")
+  val utilizationModel = new UtilizationModelDynamic(config.getDouble("cloudlet.utilizationModel"))
+}
+
